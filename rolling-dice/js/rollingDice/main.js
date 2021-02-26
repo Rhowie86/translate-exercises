1268059981879037809cb5c35262da0c1e8f87ad
Rhowie86 <ronaldnhowie@gmail.com>
2021-02-26 15:52:14 -0600

    dice rolls

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,20 @@
-// Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < 10; i++) {
+  let die1 = getRandomInt(1, 6);
+  let die2 = getRandomInt(1, 6);
+  if (die1 === die2) {
+    let doubles = die1 + die2;
+    console.log(`${die1} + ${die2} ==`, doubles, "DOUBLES!");
+  } else {
+    let result = die1 + die2;
+    console.log(`${die1} + ${die2}==`, result);
+  }
+}
